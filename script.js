f35268b4ddee1c54991baba3cd4f0c8ff25887ef
xiaoshan08 <78082919+xiaoshan08@users.noreply.github.com>
2021-05-04 14:39:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// TOPへスクロール ↓↓
+$(function(){
+    var pagetop = $('#page_top');
+    // ボタン非表示
+    pagetop.hide();
+    // 300px スクロールしたらボタン表示
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > 300) {
+            pagetop.fadeIn();
+       } else {
+            pagetop.fadeOut();
+       }
+    });
+    // 1.0秒かけてTOPへ戻る
+    pagetop.click(function () {
+       $('body, html').animate({ scrollTop: 0 }, 1000);
+       return false;
+    });
+});
+
+// 質問(PC版)、受講の流れ(SP版)のアコーディオン ↓↓
+jQuery(function ($) {
+    $('.js-accordion-title').on('click', function () {
+      /*クリックでコンテンツを開閉*/
+      $(this).next().slideToggle(200);
+      /*矢印の向きを変更*/
+      $(this).toggleClass('open', 200);
+    });
+});
+
