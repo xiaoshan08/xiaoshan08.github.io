166b5477207fc2d37e1a4f4db3a1f68f02750de6
xiaoshan08 <78082919+xiaoshan08@users.noreply.github.com>
2021-07-15 21:58:17 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+$(function(){
+
+  $('.burger-btn').on('click', function(){
+      $('.bar').toggleClass('cross');
+      $('.bar').toggleClass('white');
+      $('.header-nav').toggleClass('open');
+      $('.burger-musk').fadeToggle(300);
+  });
+
+  $('.slider').slick({
+    autoplay:true, // 自動再生を設定
+    autoplaySpeed:2000, // スライド切り替えの時間を設定
+    speed: 1000,
+    dots:true, // インジケーターを表示
+    centerMode:true,
+    centerPadding: '5%',
+  });
+
+});
