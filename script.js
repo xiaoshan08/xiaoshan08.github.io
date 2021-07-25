7912ab7637f4e727cdcb75afa082121211ab75ca
xiaoshan08 <78082919+xiaoshan08@users.noreply.github.com>
2021-07-25 12:40:36 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ $(function(){
   });
 
 });
+
+// 質問(PC版)のアコーディオン ↓↓
+jQuery(function ($) {
+  $('.js-accordion-title').on('click', function () {
+    /*クリックでコンテンツを開閉*/
+    $(this).next().slideToggle(200);
+    /*矢印の向きを変更*/
+    $(this).toggleClass('open', 200);
+  });
+});
+
+// Contact term(PC版)のアコーディオン ↓↓
+jQuery(function ($) {
+  $('.term-accordion-title').on('click', function () {
+    /*クリックでコンテンツを開閉*/
+    $(this).next().slideToggle(200);
+  });
+});
