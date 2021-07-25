53a7c8fb00df7fafee4ff5cf34e40394be97aaf8
xiaoshan08 <78082919+xiaoshan08@users.noreply.github.com>
2021-07-25 14:05:34 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -26,3 +26,16 @@ jQuery(function ($) {
     $(this).next().slideToggle(200);
   });
 });
+
+$(function(){
+
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
+
+});
